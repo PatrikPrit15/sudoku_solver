fd36a483c555fd6bf684110f186d052dafc222c3
Patrik Prit <pritrskypatrik@gmail.com>
2022-07-25 16:39:04 +0200

    sudoku

---
+++ sudoku_fast.py
@@ -0,0 +1,76 @@
+import random,time,copy
+
+board=[[[0,0] for c in range(9)] for _ in range(9)]
+
+def get_block(y,x):
+    yr=[0,0]
+    xr=[0,0]
+    if y<=2:
+        yr[1]=3
+    elif y<=5:
+        yr[0]=3
+        yr[1]=6
+    else:
+        yr[0]=6
+        yr[1]=9
+    if x<=2:
+        xr[1]=3
+    elif x<=5:
+        xr[0]=3
+        xr[1]=6
+    else:
+        xr[0]=6
+        xr[1]=9
+
+    for ny in range(yr[0],yr[1]):
+        for nx in range(xr[0],xr[1]):
+            if not (ny==y and nx==x):
+                yield ny,nx
+
+def is_pos(y,x,v):
+    for nc in range(9):
+        if board[y][nc][0]==v and nc!=x:
+            return 0 
+        if board[nc][x][0]==v and nc!=y:
+            return 0
+
+    for yb,xb in get_block(y,x):
+        if board[yb][xb][0] == v and not(yb==y and xb==x):
+            return 0
+    return 1
+
+
+print("zadaj vstupy")
+for y in range(9):
+    vstup=tuple(map(int,list(input())))
+    for x,v in enumerate(vstup):
+        board[y][x]=[v,v]
+
+start=time.perf_counter()
+print("riesenie je v procese\n")
+
+def solve(c):
+    y=c//9
+    x=c%9
+    if c==81:
+        for yv in range(9):
+            if yv in [3,6]:
+                print("~"*11)
+            for xv in range(9):
+                if xv in [3,6]:
+                    print("|",end="")
+                print(board[yv][xv][0],end="")
+            print()
+        print(f"cas bol {time.perf_counter()-start}s")
+        exit()
+    elif board[y][x][1]==0:
+        for i in range(1,10):
+            board[y][x][0]=i
+            if is_pos(y,x,i):
+                solve(c+1)
+        board[y][x][0]=0
+    else:
+        solve(c+1)
+
+
+solve(0)
---
+++ sudoku_slow.py
@@ -0,0 +1,97 @@
+import random,time,copy
+
+board=[[[0,set(range(1,10))] for c in range(9)] for _ in range(9)]
+
+def get_block(y,x):
+    yr=[0,0]
+    xr=[0,0]
+    if y<=2:
+        yr[1]=3
+    elif y<=5:
+        yr[0]=3
+        yr[1]=6
+    else:
+        yr[0]=6
+        yr[1]=9
+    if x<=2:
+        xr[1]=3
+    elif x<=5:
+        xr[0]=3
+        xr[1]=6
+    else:
+        xr[0]=6
+        xr[1]=9
+
+    for ny in range(yr[0],yr[1]):
+        for nx in range(xr[0],xr[1]):
+            if not (ny==y and nx==x):
+                yield ny,nx
+
+def colapse(y,x,v):
+    board[y][x][0]=v
+    board[y][x][1]=set([v])
+    for nc in range(9):
+        if nc!=x:
+            board[y][nc][1].discard(v)
+        if nc!=y:
+            board[nc][x][1].discard(v)
+
+    for yb,xb in get_block(y,x):
+        board[yb][xb][1].discard(v)
+
+
+print("zadaj vstupy")
+for y in range(9):
+    vstup=tuple(map(int,list(input())))
+    if vstup=="koniec":
+        break
+    for x,v in enumerate(vstup):
+        if v!=0:
+            colapse(y,x,v)
+    
+en=1
+while en:
+    en=0
+    for y in range(9):
+        for x in range(9):
+            if board[y][x][0]==0 and len(board[y][x][1])==1:
+                en=1
+                colapse(y,x,list(board[y][x][1])[0])
+
+print()
+board_old=copy.deepcopy(board)
+dead_count=0
+mpoc=0
+while 1:
+    mi=10
+    xmi=-1
+    ymi=-1
+    poc=0
+    for y in range(9):
+        for x in range(9):
+            l=len(board[y][x][1])
+            if l<1:
+                dead_count+=1
+                # print(f"dead {dead_count} times")
+                board=copy.deepcopy(board_old)
+            elif l>1 and l<mi:
+                mi=l
+                ymi=y
+                xmi=x
+            if l==1:
+                poc+=1
+    if poc>=mpoc:
+        mpoc=poc
+        print(mpoc,mi,xmi,ymi,dead_count)
+    if poc==81:
+        break
+    if mi<10:
+        colapse(ymi,xmi,random.choice(list(board[ymi][xmi][1])))
+
+
+for r in board:
+    print(*[e[0] for e in r])
+    
+
+
+
